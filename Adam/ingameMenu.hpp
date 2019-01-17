9f37c644d5367921bfb14ed5d163db537e4aa293
gian <gianluca.piccardo@student.hu.nl>
2019-01-17 11:54:17 +0100

    ingamemenu.hpp changes

--- Adam/ingameMenu.hpp
+++ Adam/ingameMenu.hpp
@@ -2,14 +2,16 @@
 #define INGAMEMENU_HPP
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "menu.hpp"
 
 #define pauseOptions 4
 
-class inGameMenu
+class inGameMenu: public Menu
 {
 private:
 	float width;
 	float heigth;
+	int selectedItem;
 	sf::Font font;
 	sf::Text pauseScreen[pauseOptions];
 	sf::Sprite sprite;
@@ -25,9 +27,30 @@ public:
 		pauseScreen[0].setFont(font);
 		pauseScreen[0].setString("Continue Game");
 		pauseScreen[0].setFillColor(sf::Color::Red);
-		pauseScreen[0]
+		pauseScreen[0].setCharacterSize(75);
+
+		pauseScreen[1].setFont(font);
+		pauseScreen[1].setString("Load Game");
+		pauseScreen[1].setFillColor(sf::Color::White);
+		pauseScreen[1].setCharacterSize(75);
+
+		pauseScreen[2].setFont(font);
+		pauseScreen[2].setString("Options");
+		pauseScreen[2].setFillColor(sf::Color::White);
+		pauseScreen[2].setCharacterSize(75);
+
+		pauseScreen[3].setFont(font);
+		pauseScreen[3].setString("Quit Game");
+		pauseScreen[3].setFillColor(sf::Color::White);
+		pauseScreen[3].setCharacterSize(75);
+
+		selectedItem = 0;
 	}
 
+	void draw(sf::RenderWindow & window) override;
+	void moveUp() override;
+	void moveDown() override;
+	void chooseTile(int & titleSelect) override;
 };
 
 
