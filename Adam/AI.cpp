020737cb5ccbfee2d79a62eade2b4b8373746a7a
vera98x <v.schoonderwoerd@outlook.com>
2019-01-22 10:39:35 +0100

    enemy follows better

--- Adam/AI.cpp
+++ Adam/AI.cpp
@@ -7,6 +7,7 @@ void AI::shouldFollow_followDirection(fighter * p1, fighter & p2) {
 	//Check if p1 and p2 are close to each other
 	// if so, make enemy move towarts the player
 	if ((p1->getPosition() - p2.getPosition()).x <= 50 && (p1->getPosition() - p2.getPosition()).x >= 0) {
+		p1->updateFollowPosition(0);
 		p2.health.sub(1);
 		p1->fight();
 	}
@@ -16,6 +17,8 @@ void AI::shouldFollow_followDirection(fighter * p1, fighter & p2) {
 		return;
 	}
 	else if ((p1->getPosition() - p2.getPosition()).x >= -50 && (p1->getPosition() - p2.getPosition()).x <= -0) {
+		p1->updateFollowPosition(0);
+		p2.health.sub(1);
 		p1->fight();
 	}
 	else if ((p1->getPosition() - p2.getPosition()).x >= -400 && (p1->getPosition() - p2.getPosition()).x <= -50) {
