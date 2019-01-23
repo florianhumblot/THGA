cffed881901bdd36bc22d9e66018fba9ffba2fdc
gian <gianluca.piccardo@student.hu.nl>
2019-01-23 13:05:03 +0100

    new audio cpp hpp

--- Adam/Audio.cpp
+++ Adam/Audio.cpp
@@ -1,1 +1,11 @@
 #include "pch.h"
+#include "audio.hpp"
+
+Audio::Audio(const std::string &pathName)
+{
+	if (!buffer.loadFromFile(pathName))
+	{
+		std::cout << "Error loading sound from memory." << std::endl;
+	}
+	sound.setBuffer(buffer);
+}
--- Adam/Audio.hpp
+++ Adam/Audio.hpp
@@ -1,6 +1,19 @@
 #ifndef AUDIO_HPP
 #define AUDIO_HPP
 
+#include "pch.h"
 
 
-#ENDIF //AUDIO_HPP
+class Audio
+{
+private:
+	sf::SoundBuffer buffer;
+	sf::Music music;
+	sf::Sound sound;
+	std::string pathName;
+	std::vector<Audio> audioGroup;
+public:
+	Audio(const std::string &pathName);
+};
+
+#endif //AUDIO_HPP
