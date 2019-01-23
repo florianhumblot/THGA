f5c716d14656c7f981bc8971b9a18e83218440c9
MarkGasse <mark.gasse@student.hu.nl>
2019-01-23 13:04:46 +0100

    bounce object

--- Adam/levelManager.h
+++ Adam/levelManager.h
@@ -10,15 +10,20 @@ private:
 	sf::Texture tex;
 	sf::Texture tex2;
 	sf::Texture tex3;
+	sf::Texture tex4;
 
 
 
 public:
+	sf::Vector2f spawnpoint_player;
 	sf::Sprite bg;
 	sf::Sprite fg_dmg;
+	sf::Sprite fg_bounce;
 	sf::Sprite background;
 	sf::Sprite damage_background;
 	sf::Sprite ground;
+	sf::Sprite foreground_bounce;
+	std::map<std::string, std::map<int, std::string> > spawnpoints_enemys;
 
 	std::map<std::string, std::map<std::string, std::string> > lvls;
 	levelManager();
